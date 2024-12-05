1431a1e2aca7d56f53d63821224d5005f90ca1d4
AzRom <azromjr@gmail.com>
2024-12-05 18:48:01 +0300

    another commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   typescript: {
     ignoreBuildErrors: true,
-=======
-    eslint: {
-    ignoreDuringBuilds: true,
->>>>>>> 28ebb9d8ace2a2731520d253f2b8fce517e47a4d
   },
     images: {
     domains: ['firebasestorage.googleapis.com'],
